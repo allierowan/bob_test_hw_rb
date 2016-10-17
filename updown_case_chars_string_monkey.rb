8b496adc6db909cae2b686cf336f5da39250f59c
Aileen Rowan <aileen.s.rowan@gmail.com>
2016-10-17 15:12:46 -0400

    added comments

--- updown_case_chars_string_monkey.rb
+++ updown_case_chars_string_monkey.rb
@@ -1,13 +1,16 @@
 class String
 
+  # returns an array of all the capital letters in a string
   def get_cap_chars
     chars & ("A".."Z").to_a
   end
 
+  # returns an array of all the lower case letters in a string
   def get_lower_chars
     chars & ("a".."z").to_a
   end
 
+  # checks if the string has letters and that they're all uppercase
   def has_only_uppercase_chars
     !get_cap_chars.empty? && get_lower_chars.empty?
   end
